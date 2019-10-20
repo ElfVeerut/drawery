57874d6aa07716264cfcbf0cc9e2f0a3d4442c9f
Elf <42993063+ElfVeerut@users.noreply.github.com>
2019-10-20 19:39:54 +0700

    Add files via upload

--- fingerprint/checkm1.py
+++ fingerprint/checkm1.py
@@ -1,4 +1,3 @@
-
 """
 
 PyFingerprint
@@ -6,65 +5,65 @@ PyFingerprint
 Copyright (C) 2015 Bastian Raschke <bastian.raschke@posteo.de>
 
 All rights reserved.
-
+https://gitmemory.com/issue/bastianraschke/pyfingerprint/70/480343013
 """
-
 
 import hashlib
 
 from pyfingerprint.pyfingerprint import PyFingerprint
 
+def check():
 
-## Tries to search the finger and calculate hash
+    ## Tries to search the finger 
 
-try:
+    try:
 
-    finger = PyFingerprint('/dev/ttyUSB0')
-
-
-    print('Waiting for finger...')
+        finger = PyFingerprint('/dev/ttyUSB0')
 
 
-    ## Wait that finger is read
-
-    while ( finger.readImage() == False ):
-
-        pass
+        print('Waiting for finger...')
 
 
-    ## Converts read image to characteristics and stores it in charbuffer 1
+        ## Wait that finger is read
 
-    finger.convertImage(0x01)
+        while ( finger.readImage() == False ):
 
-
-    ## Searchs template
-
-    result = finger.searchTemplate()
+            pass
 
 
-    positionNumber = result[0]
+        ## Converts read image to characteristics and stores it in charbuffer 1
 
-    accuracyScore = result[1]
-
-
-    if ( positionNumber == -1 ):
-
-        print('no match found!')
-
-       # return 'no match found!'
-
-    else:
-
-        print('Found template at position #' + str(positionNumber))
-
-        print('The accuracy score is: ' + str(accuracyScore))
+        finger.convertImage(0x01)
 
 
+        ## Searchs template
 
-except Exception as e:
+        result = finger.searchTemplate()
+        print(result)
 
-    print('Operation failed!')
 
-    print('Exception message: ' + str(e))
+        positionNumber = result[0]
 
-    exit(1)
+        accuracyScore = result[1]
+
+
+        if ( positionNumber == -1 ):
+            return (False)
+            print('no match found!')
+
+
+        else:
+            return (True)
+            print('Found template at position #' + str(positionNumber))
+
+            
+
+
+
+    except Exception as e:
+        return (False)
+        print('Operation failed!')
+
+        print('Exception message: ' + str(e))
+
+        exit(1)   
