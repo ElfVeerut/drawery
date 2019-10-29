ddc66a70926f39ecc8c3ba2f9340b7405f7f3024
Elf <42993063+ElfVeerut@users.noreply.github.com>
2019-10-29 00:35:36 -0500

    Add files via upload

--- stepmotor/Home_pos.py
+++ stepmotor/Home_pos.py
@@ -11,12 +11,12 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 class HomePosition(threading.Thread):
     def __init__(self,dir_motor,step_motor,pos_home,CW_CCW):
+        threading.Thread.__init__(self)
         self.dir_motor = dir_motor
         self.step_motor = step_motor
         self.pos_home = pos_home
         self.value_switch = GPIO.input(self.pos_home)
         self.CW_CCW = CW_CCW
-        self.home = False
         GPIO.setup(self.dir_motor, GPIO.OUT)
         GPIO.setup(self.step_motor, GPIO.OUT)
     def limit_switch(self):
@@ -24,15 +24,17 @@ class HomePosition(threading.Thread):
         return self.value_switch
     def run(self): 
         GPIO.output(self.dir_motor,self.CW_CCW)
-        temp =  
-        while not self.limit_switch():
-            try:
+        temp =  self.limit_switch()
+        try:
+            while not self.limit_switch(): 
                 GPIO.output(self.step_motor,GPIO.HIGH)
                 sleep(.001)
                 GPIO.output(self.step_motor,GPIO.LOW)
                 sleep(.001)
-            except:
-                if temp == 
+        except:
+            print(self.limit_switch())
+        if temp == self.limit_switch():
+            return True
 x = HomePosition(19,26,4,CW)
 y = HomePosition(20,21,17,CCW)
 z = HomePosition(13,6,18,CCW)
@@ -41,9 +43,9 @@ x.start()
 y.start()
 z.start()
 
-x_t.join()
-y_t.join()
-z_t.join()
+x.join()
+y.join()
+z.join()
 
 GPIO.cleanup()
 print("HOME")
