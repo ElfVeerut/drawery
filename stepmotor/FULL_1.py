8c47e212fdde040575c668afa24dd64102afcbb2
Elf <42993063+ElfVeerut@users.noreply.github.com>
2019-11-06 17:15:56 +0700

    Add files via upload

--- stepmotor/FULL_1.py
+++ stepmotor/FULL_1.py
@@ -48,8 +48,12 @@ class HomePosition(threading.Thread):
             sleep(0.001)
             GPIO.output(self.step_motor,GPIO.LOW)
             sleep(0.001)
-        print("DONE")
+        return "DONE"
 def prepare_pos():
+    x = HomePosition(19,26,4,CW)
+    y = HomePosition(20,21,17,CCW)
+    z = HomePosition(13,6,18,CCW)
+
     motor_threading1 = threading.Thread(target=x.move,args=(1000,CCW)) #1000
     motor_threading2 = threading.Thread(target=y.move,args=(1600,CW)) #1675
     motor_threading3 = threading.Thread(target=z.move,args=(100,CW))
@@ -59,36 +63,63 @@ def prepare_pos():
     motor_threading1.join()
     motor_threading2.join()
     motor_threading3.join()
-    print("Prepare positon")
+    return "Prepare positon"
+    
 def out():
+    x = HomePosition(19,26,4,CW)
+    y = HomePosition(20,21,17,CCW)
+    z = HomePosition(13,6,18,CCW)
+
     z.move(700,CW)
     sleep(0.05)
     x.move(3000,CW)
     sleep(0.05)
+    return "OUT"
 def lift():
+    x = HomePosition(19,26,4,CW)
+    y = HomePosition(20,21,17,CCW)
+    z = HomePosition(13,6,18,CCW)
+
     x.move(2300,CCW)
     sleep(0.05)
     z.move(500,CW)
     sleep(0.05)
     x.move(3000,CW)
-
+    return "LIFT"
 def go_home():
+    x = HomePosition(19,26,4,CW)
+    y = HomePosition(20,21,17,CCW)
+    z = HomePosition(13,6,18,CCW)
 
     x.start()
     y.start()
     z.start()
-
     x.join()
     y.join()
     z.join()
+    return "HOME"
+def go_home2():
+    x = HomePosition(19,26,4,CW)
+    y = HomePosition(20,21,17,CCW)
+    z = HomePosition(13,6,18,CCW)
 
+    motor_threading1 = threading.Thread(target=x.run,args=()) #1000
+    motor_threading2 = threading.Thread(target=y.run,args=()) #1675
+    motor_threading3 = threading.Thread(target=z.run,args=())
+    motor_threading1.start()
+    motor_threading2.start()
+    motor_threading3.start()
+    motor_threading1.join()
+    motor_threading2.join()
+    motor_threading3.join()
+    return "HOME2"
 #GPIO.cleanup()
-x = HomePosition(19,26,4,CW)
-y = HomePosition(20,21,17,CCW)
-z = HomePosition(13,6,18,CCW)
-
 
 def go_to_locker(n):
+    x = HomePosition(19,26,4,CW)
+    y = HomePosition(20,21,17,CCW)
+    z = HomePosition(13,6,18,CCW)
+
     if n == 1 :
         y.move(1450,CW)
         print("reach locker bot_left")
@@ -147,6 +178,10 @@ def go_to_locker(n):
         sleep(0.05)
         z.move(700,CCW)
 def returnpos_to_locker(n):
+    x = HomePosition(19,26,4,CW)
+    y = HomePosition(20,21,17,CCW)
+    z = HomePosition(13,6,18,CCW)
+
     if n == 4:
         out()
         z.move(2600,CCW)
@@ -157,6 +192,7 @@ def returnpos_to_locker(n):
         sleep(0.05)
         x.move(3000,CW)
         sleep(1)
+        return "DONE"
     elif n == 3:
         out()
         sleep(0.05)
@@ -196,3 +232,6 @@ def returnpos_to_locker(n):
         z.move(500,CCW)
         sleep(0.05)
         x.move(3000,CW)
+go_home()
+print("HOME")
+prepare_pos()
