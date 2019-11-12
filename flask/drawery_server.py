63598ebc210ff9bff48de8354211b5c49d5efcf9
Elf <42993063+ElfVeerut@users.noreply.github.com>
2019-11-12 22:06:57 +0700

    Add files via upload

--- flask/drawery_server.py
+++ flask/drawery_server.py
@@ -1,22 +1,43 @@
 from flask import Flask, redirect, url_for, request, render_template, jsonify, request
 import ast, json, os, webbrowser,csv
 from time import sleep
+from dear import *
 from enrollm1 import enroll
 from checkm1 import check
 from weight_check import checkWeight
-#from new_finger_track.csv import csv
+import csv
 #import panda as pd
 
-#filename = "new_finger_track.csv"
-#header = ("ID","Name","Item","Public/Private","Availability","Time")
-#with open(filename,'r', newline= "") as file:
+filename = "new_finger_track.csv"
+header = ("ID","Name","Item","Public/Private","Availability","Time")
+def recall_data():
+    global box
+    global public_box
+    global private_box
+    box = []
+    public_box = []
+    private_box = []
+    with open(filename,'r', newline= "") as file:
+        reader = csv.reader(file, delimiter=',')
+        i=0
+        for line in reader:
+            if i == 0: i+=1; continue
+            box.append(int(line[4]))
+        print(box)
+        public_box = [box[0],box[1]]
+        private_box = [box[2],box[3]]
+    
+    return ('done')
+
+def rewrite_data()
+       
 
 #    readData = [row for row in csv.DictReader(file)]
 
-
-#go_home()
-#sleep(0.5)
-#prepare_post()
+go_home()
+sleep(0.5)
+prepare_pos()
+recallData()
 data = '0'
 withdrawdata = '0'
 app = Flask(__name__)
@@ -24,6 +45,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('welcome.html')
+
+@app.route('/withdraw_or_deposit')
+def WithdrawOrDeposit():
+    return render_template('WithdrawOrDeposit.html')
 
 @app.route('/deposit_available')
 def available():
@@ -36,10 +61,6 @@ def public_available():
 @app.route('/deposit_private_available')
 def private_available():
     return render_template('Deposit_Private_Available.html')
-
-@app.route('/withdrawal')
-def withdrawal():
-    return render_template('Withdraw_Available.html')
 
 @app.route('/check_finger')
 def checkFinger():
@@ -65,20 +86,29 @@ def ReturnItemWithdraw():
 def ReturnItemPrivate():
     return render_template('return_drawer_private.html')
 
+@app.route('/withdrawal')
+def withdrawal():
+    recall_data()
+    if box != [1,1,1,1]:
+        return render_template('Withdraw_Available.html')
+    else:
+        return render_template('NoAvailableForEveryDrawer.html')
+    return ('error')
+
 @app.route('/public_or_private')
 def PublicOrPrivate():
-    box = []
+    recall_data()
+    print(box)
+#    box = [0,0,0,0]    
     if box == [1,1,1,1]:
         return render_template('NoAvailableForEveryDrawer.html')
     else:
         return render_template('Private_public.html')
-#    for _ in  
-    
  
 @app.route('/name_deposit', methods = ['GET','POST'])
 def nameDeposit():
     res = ''
-    nc=[0,0,1,1]
+    nc=[0,0,0,0]
     if request.method == 'POST':
         name = request.form['FirstName']
         res += name       
@@ -88,7 +118,8 @@ def nameDeposit():
         
 @app.route('/box_check_dp')
 def check_dp():
-    public_box = [0,1]
+    recall_data()
+#    public_box = [0,0]
 #    x = HomePosition(19,26,4,CW)
 #    y = HomePosition(20,21,17,CWW)
 #    z = HomePosition(13,6,18,CW)
@@ -115,13 +146,15 @@ def check_dp():
                
 @app.route('/box_check_public_deposit', methods = ['GET','POST'])
 def public_deposit():
-    public_box = [0,1]
+    recall_data()
+#    public_box = [0,0]
     if request.method == 'GET':
         return json.dumps(public_box)
         
 @app.route('/box_check_pp')
 def check_pp():
-    private_box = [1,0]
+    recall_data()
+#    private_box = [0,0]
 #    x = HomePosition(19,26,4,CW)
 #    y = HomePosition(20,21,17,CWW)
 #    z = HomePosition(13,6,18,CW)
@@ -148,49 +181,53 @@ def check_pp():
 
 @app.route('/get_drawer_private', methods = ['POST'])
 def getDrawerPrivate():
-    private_box = [1,0]
+    recall_data()
+#    private_box = [0,0]
     if request.method == 'POST':
         if private_box[0] == 0:
-#            go_to_locker(2)
+            go_to_locker(2)
             return render_template('place_item_private.html') #return page GUI
             
         elif private_box[1] == 0:
-#            go_to_locker(1)
+            go_to_locker(1)
             return render_template('place_item_private.html') #return page GUI
     return('error')
 
 @app.route('/return_drawer_private', methods = ['POST'])
 def returnDrawerPrivate():
-    private_box = [1,0]
+    recall_data()
+#    private_box = [0,0]
     if request.method == 'POST':
         if private_box[0] == 0:
-#            returnpos_to_locker(2)
-#            sleep(1)
-#            go_home()
-#            prepare_pos()
+            returnpos_to_locker(2)
+            sleep(1)
+            go_home()
+            prepare_pos()
             return render_template('welcome.html')
 #            return ('finish') #return page GUI
             
         elif private_box[1] == 0:
-#            returnpos_to_locker(1)
-#            sleep(1)
-#            go_home()
-#            prepare_pos()
+            returnpos_to_locker(1)
+            sleep(1)
+            go_home()
+            prepare_pos()
             return render_template('welcome.html')
 #            return ('finish')
     return ('error')
 
 @app.route('/box_check_private_deposit', methods = ['GET','POST'])
 def private_deposit():
-    private_box = [1,0]
+    recall_data()
+#    private_box = [0,0]
     if request.method == 'GET':        
         return json.dumps(private_box)
                 
 @app.route('/name_withdrawal', methods = ['GET'])
 def nameWithdrawal():
-    name_withdraw = [0,1,1,0]
+    recall_data()
+#    name_withdraw = [0,0,0,0]
     if request.method == 'GET':
-        return json.dumps(name_withdraw)
+        return json.dumps(box)
         
 @app.route('/register', methods = ['POST'])
 def enroll_():
@@ -234,7 +271,7 @@ def getRegisterName():
         print (register_name)
         return render_template('fingerprint_register.html')
     
-@app.route('/weight_check', methods = ['POST'])
+@app.route('/weight_check_or_button', methods = ['POST'])
 def check_weight():
     if request.method == 'POST':
         checkweight = request.form['weight']
@@ -260,7 +297,7 @@ def get_drawers():
         data = int(data)
 #        name = request.form['getdrawer']
 #        print(name)
-#        go_to_locker(data)
+        go_to_locker(data)
         return render_template('Place_item.html')  
     return(str(data))
 
@@ -272,10 +309,11 @@ def return_drawers():
     if request.method == 'POST':
         data = int(data)
         print(data)
-#        returnpos_to_locker(data)
-#        sleep(1)
-#        go_home()
-#        prepare_pos()
+        returnpos_to_locker(data)
+        sleep(1)
+        go_home()
+        prepare_pos()
+        recall_data()
             
         return render_template('welcome.html')
 
@@ -294,7 +332,7 @@ def get_withdraw_drawer():
     print(withdrawdata)
     if request.method == 'POST':
         withdrawdata = int(withdrawdata)
-#        go_to_locker(withdrawdata)
+        go_to_locker(withdrawdata)
         return render_template('place_item_withdraw.html')
 #        return (str(withdrawdata))
 
@@ -306,15 +344,15 @@ def return_drawers_withdraw():
     if request.method == 'POST':
         withdrawdata = int(withdrawdata)
         print(withdrawdata)
-#        returnpos_to_locker(withdrawdata)
-#        sleep(1)
-#        go_home()
-#        prepare_pos()
+        returnpos_to_locker(withdrawdata)
+        sleep(1)
+        go_home()
+        prepare_pos()
+        recall_data()
             
         return render_template('welcome.html')
         
-           
-        
+
         
         
         
